a8f9a05ba997f68c429b7bc48c7dbe29f4580add
Paweł Stefański <qnick.mail@gmail.com>
2021-03-08 22:48:54 +0100

    remove old tests, new build process for app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // running npm-gui development version
-const npmGuiServer = require('./dist/server/main.js').default;
+const {start} = require('./dist/index.js');
 
 const args = process.argv.slice(2);
 let host = null;
@@ -10,4 +10,4 @@ if (args[0]) {
   host = args[0].split(':')[0];
   port = args[0].split(':')[1];
 }
-npmGuiServer(host, port);
+start(host, port);
--- server/middlewares/unmatched.ts
+++ server/middlewares/unmatched.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from 'express';
 import path from 'path';
 
 export const unmatchedHandler: RequestHandler = (_, res) => {
-  res.sendFile(path.join(__dirname, '/../client', 'index.html'));
+  res.sendFile(path.join(__dirname, '../', 'index.html'));
 };
