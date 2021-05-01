781edd357eef06ef67ec99d029cb4c6922f54169
Paweł Stefański <qnick.mail@gmail.com>
2021-05-01 14:11:47 +0200

    fix v install

--- install-correct-pnpm-version.js
+++ install-correct-pnpm-version.js
@@ -1,7 +1,7 @@
 const { spawnSync } = require("child_process");
 
 const version = /v(\d+)\./.exec(process.version);
-if (+version[1] >= 12) {
+if (+version[1] > 12) {
   spawnSync('npm',['install','-g', 'pnpm']).output.toString();
 } else {
   spawnSync('npm',['install','-g', 'pnpm@5']).output.toString();
