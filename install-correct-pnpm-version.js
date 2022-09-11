b21bc5a1928461fcd567164825b80538b5aa2e6b
pst <pawel@devnullapps.com>
2022-09-11 21:16:46 +0200

    fix for node 12, 14

--- install-correct-pnpm-version.js
+++ install-correct-pnpm-version.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 const { execSync } = require('child_process');
 
-const version = /v(?<major>\d+)\./.exec(process.version);
+const version = /v(?<major>\d+)/.exec(process.version);
 console.log('installing:', version, +version.groups.major, process.version);
-if (+version.groups.major > 12) {
+if (+version.groups.major > 14) {
   console.log(execSync('npm install -g pnpm').toString());
+} else if (+version.groups.major > 12) {
+  console.log(execSync('npm install -g pnpm@6').toString());
 } else {
   console.log(execSync('npm install -g pnpm@5').toString());
 }
